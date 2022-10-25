532425606b27a4745dab18b84295d6ad43a769da
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-10-25 17:38:29 +0200

    adding bootstrap

---
+++ src/main/java/com/dzmitry/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,45 @@
+package com.dzmitry.msscbeerservice.bootstrap;
+
+import com.dzmitry.msscbeerservice.domain.Beer;
+import com.dzmitry.msscbeerservice.repository.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObject();
+    }
+
+    private void loadBeerObject() {
+        if(beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityOnHand(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityOnHand(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
