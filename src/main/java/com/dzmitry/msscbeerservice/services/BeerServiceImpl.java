53a93e71bcd017f5108e9843e82f1bdc5c63f2be
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-01 09:41:54 +0100

    adding ehcache to listBeer and getById methods

--- src/main/java/com/dzmitry/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/dzmitry/msscbeerservice/services/BeerServiceImpl.java
@@ -7,6 +7,8 @@ import com.dzmitry.msscbeerservice.web.model.BeerDto;
 import com.dzmitry.msscbeerservice.web.model.BeerPagedList;
 import com.dzmitry.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, boolean showInventoryOnHand) {
 
@@ -66,6 +69,7 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false")
     @Override
     public BeerDto getById(UUID beerId, boolean showInventoryOnHand) {
         if(showInventoryOnHand) {
